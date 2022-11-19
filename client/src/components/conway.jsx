01633a51ef318beb4e2923dbce849debd8ac67c7
Tucker Whitaker <trw2005.50@gmail.com>
2022-11-19 14:14:36 -0600

    conway game of life2

--- client/src/components/conway.jsx
+++ client/src/components/conway.jsx
@@ -1,7 +1,110 @@
 import "./css/conway.css";
+import React, { useState } from "react";
+let w = 200;
+let G = 0;
 
-let w = 100;
-function conway() {
+function Conway() {
+	const Con = () => {
+		let state = [];
+		for (let i = 0; i < w; i++) {
+			state.push([]);
+
+			for (let j = 0; j < w; j++) {
+				if (Math.floor(Math.random() * 11) > 8) {
+					state[i].push(1);
+				} else {
+					state[i].push(0);
+				}
+			}
+		}
+
+		async function update() {
+			for (let i = 0; i < w; i++) {
+				for (let j = 0; j < w; j++) {
+					if (state[i][j] == 0) {
+						ctx.clearRect(i, j, 1, 1);
+					} else if (state[i][j] == 1) {
+						//white
+						//#FFFFFF
+
+						//gray
+						//#646464
+						ctx.fillStyle = "#FFFFFF";
+						ctx.fillRect(i, j, 1, 1);
+					}
+				}
+			}
+
+			let newstate = [];
+			for (let i = 0; i < w; i++) {
+				newstate.push([]);
+			}
+
+			for (let i = 0; i < w; i++) {
+				for (let j = 0; j < w; j++) {
+					let n = 0;
+
+					if (i == 0 || i == w - 1 || j == 0 || j == w - 1) {
+						newstate[i][j] = 0;
+					} else {
+						n += state[i + 1][j];
+						n += state[i + 1][j + 1];
+						n += state[i + 1][j - 1];
+						n += state[i][j + 1];
+						n += state[i - 1][j + 1];
+						n += state[i - 1][j - 1];
+						n += state[i - 1][j];
+						n += state[i][j - 1];
+
+						if (state[i][j] == 0 && n == 3) {
+							newstate[i][j] = 1;
+						} else if (state[i][j] == 1) {
+							if (n < 2 || n > 3) {
+								//black
+								newstate[i][j] = 0;
+							}
+						}
+						if (newstate[i][j] == null) {
+							newstate[i][j] = state[i][j];
+						}
+					}
+				}
+			}
+
+			/*for (let i = 0; i < w; i++) {
+				for (let j = 0; j < w; j++) {
+					if (state[i][j] == 0) {
+						ctx.fillStyle = "#000000";
+						ctx.fillRect(i, j, 1, 1);
+					} else if (state[i][j] == 1) {
+						//white
+						ctx.fillStyle = "#FFFFFF";
+						ctx.fillRect(i, j, 1, 1);
+					}
+				}
+			}*/
+
+			await delay(100);
+			state = newstate;
+			G++;
+
+			if (G < 1000000) {
+				update();
+			}
+		}
+
+		function delay(time) {
+			return new Promise((resolve) => setTimeout(resolve, time));
+		}
+
+		var c = document.getElementById("Canvas");
+		var ctx = c.getContext("2d");
+		ctx.moveTo(0, 0);
+
+		ctx.fillStyle = "#FF0000";
+		update();
+	};
+
 	return (
 		<div className="Parent">
 			<button
@@ -26,46 +129,4 @@ var c = document.getElementById("Canvas");
     ctx.clearRect(0, 0, w, w);
 */
 
-let G = 0;
-const Con = () => {
-	let r = 0;
-	async function update() {
-		ctx.moveTo(0, 0);
-		ctx.clearRect(0, 0, w, w);
-		ctx.fillStyle = "#000000";
-		ctx.fillRect(0, 0, w, w);
-		for (let i = 0; i < w; i++) {
-			for (let j = 0; j < w; j++) {
-				if (r == i * w + j) {
-					ctx.fillStyle = "#FF0000";
-					ctx.fillRect(i + 1, j, 1, 1);
-					ctx.fillRect(i - 1, j, 1, 1);
-					ctx.fillRect(i, j + 1, 1, 1);
-					ctx.fillRect(i, j - 1, 1, 1);
-
-					ctx.fillStyle = "#ffffff";
-					ctx.fillRect(i, j, 1, 1);
-				}
-			}
-		}
-		await delay(1);
-		G++;
-		r++;
-		if (G < 10000) {
-			update();
-		}
-	}
-
-	function delay(time) {
-		return new Promise((resolve) => setTimeout(resolve, time));
-	}
-
-	var c = document.getElementById("Canvas");
-	var ctx = c.getContext("2d");
-	ctx.moveTo(0, 0);
-
-	ctx.fillStyle = "#000000";
-	update();
-};
-
-export default conway;
+export default Conway;
